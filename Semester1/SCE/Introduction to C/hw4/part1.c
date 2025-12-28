0d8e91798d0b870a5678e4f908645d849d9af888
Gezraf <59087430+Gezraf@users.noreply.github.com>
2025-12-28 13:07:31 +0200

    Format code and improve indentation in part1.c

--- Semester1/SCE/Introduction to C/hw4/part1.c
+++ Semester1/SCE/Introduction to C/hw4/part1.c
@@ -1,51 +1,51 @@
-/* Assignment: 4
-Author: Guy Shitrit, ID: 330707761
-*/
+    /* Assignment: 4
+    Author: Guy Shitrit, ID: 330707761
+    */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
 
 
-int floor(float num) { // פונקציית עזר שמחזירה את המספר השלם הקרוב והקטן ביותר למספר העשרוני שנשלח ע"י המרה למספר שלם
-    return (int) num;
-}
-
-float getDecimal(float num) { // פונקציית עזר שמחזירה את ה"חלק השבור" של המספר העשרוני לפי הפרש המספר בחלקו השלם
-    return num - floor(num);
-}
-
-float* sums(float arr[], int size) {
-    float integerSum = 0;
-    float decimalSum = 0;
-
-    float* ptr = (float*)malloc(2*sizeof(float)); // הקצאת זיכרון לשתי מספרים ממשיים כדי לשמור את שני התוצאות
-
-    for (int i = 0; i<size; i++) {
-        integerSum += floor(arr[i]);
-        decimalSum += getDecimal(arr[i]);
+    int floor(float num) { // פונקציית עזר שמחזירה את המספר השלם הקרוב והקטן ביותר למספר העשרוני שנשלח ע"י המרה למספר שלם
+        return (int) num;
     }
 
-    ptr[0] = integerSum; // שמירת תוצאת סכום החלקים השלמיים בתא הזיכרון הראשון
-    ptr[1] = decimalSum; // שמירת תוצאת סכום החלקים ה"שבורים" בתא הזיכרון השני
-
-    return ptr;
-}
-
-int main() {
-    int size;
-    printf("Enter the size of numbers in the array: ");
-    scanf("%d", &size); // קלט גודל המערך מהמשתמש
-
-    float arr[size];
-    for (int i = 0; i<size; i++) {
-        printf("Insert number into array: ");
-        scanf("%f", &arr[i]); // קלט מספרים ממשיים לתוך המערך
+    float getDecimal(float num) { // פונקציית עזר שמחזירה את ה"חלק השבור" של המספר העשרוני לפי הפרש המספר בחלקו השלם
+        return num - floor(num);
     }
 
-    float* ptr = sums(arr, size); // המצביע שאמור לקבל את שני התוצאות
-    printf("%d , %f", (int) ptr[0], ptr[1]);
-    free(ptr); // לא שכחתי לשחרר (:
+    float* sums(float arr[], int size) {
+        float integerSum = 0;
+        float decimalSum = 0;
 
-    return 0;
-}
+        float* ptr = (float*)malloc(2*sizeof(float)); // הקצאת זיכרון לשתי מספרים ממשיים כדי לשמור את שני התוצאות
+
+        for (int i = 0; i<size; i++) {
+            integerSum += floor(arr[i]);
+            decimalSum += getDecimal(arr[i]);
+        }
+
+        ptr[0] = integerSum; // שמירת תוצאת סכום החלקים השלמיים בתא הזיכרון הראשון
+        ptr[1] = decimalSum; // שמירת תוצאת סכום החלקים ה"שבורים" בתא הזיכרון השני
+
+        return ptr;
+    }
+
+    int main() {
+        int size;
+        printf("Enter the size of numbers in the array: ");
+        scanf("%d", &size); // קלט גודל המערך מהמשתמש
+
+        float arr[size];
+        for (int i = 0; i<size; i++) {
+            printf("Insert number into array: ");
+            scanf("%f", &arr[i]); // קלט מספרים ממשיים לתוך המערך
+        }
+
+        float* ptr = sums(arr, size); // המצביע שאמור לקבל את שני התוצאות
+        printf("%d , %f", (int) ptr[0], ptr[1]);
+        free(ptr); // לא שכחתי לשחרר (:
+
+        return 0;
+    }

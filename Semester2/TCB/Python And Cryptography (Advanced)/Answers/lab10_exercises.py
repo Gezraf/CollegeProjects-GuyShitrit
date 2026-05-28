ba955dca6f92fb5abaf98f319cfde73e045b7d68
Gezraf <59087430+Gezraf@users.noreply.github.com>
2026-05-28 12:58:32 +0300

    Implement substitution encryption and enhance zigzag methods
    Added substitution_encrypt function and improved zigzag_encrypt logic.

--- Semester2/TCB/Python And Cryptography (Advanced)/Answers/lab10_exercises.py
+++ Semester2/TCB/Python And Cryptography (Advanced)/Answers/lab10_exercises.py
@@ -1,12 +1,21 @@
+def substitution_encrypt(text):
+    cipher = ""
+    for c in text:
+        new = 127-ord(c)
+        new_char = chr(new)
+        cipher = cipher + new_char
+
+    return cipher
+
 def transposition(text):
     return text[::-1]
 
 
 def zigzag_preprocess(text):
-    strip = text.strip()
+    strip = text.replace(' ', '')
     n = len(strip)
 
-    mat = [[''] * n] + [[''] * n]
+    mat = [[''] * n for _ in range(2)]
 
     i, j = 0, 0
 
@@ -22,23 +31,46 @@ def zigzag_preprocess(text):
 
     return mat
 
-def zigzag_encrpyt(text):
+
+def zigzag_encrypt(text):
+    space_positions = [i for i in range(len(text)) if text[i] == ' ']
     mat = zigzag_preprocess(text)
 
-    func = lambda c: c != ''
-    line1, line2 = ''.join(list(filter(func, mat[0]))), ''.join(list(filter(func, mat[1])))
-    res = ''
+    processLine = lambda arr: ''.join(list(filter(lambda c: c != '', arr)))
+    line1, line2 = processLine(mat[0]), processLine(mat[1])
+    string = (line1 + line2)
 
-    for c in line1:
-        res += c
-    for c in line2:
-        res += c
-
-    res = ''
+    res = ' '.join([string[i:i+4] for i in range(0, len(string), 4)])
 
     for line in mat:
         print(line)
 
-    return res
+    return res, space_positions
 
-print(zigzag_encrpyt(transposition("אני מגיע רק מחרתיים")))
+def zigzag_decrypt(encrypted_text, spaces):
+    encrypted_text = encrypted_text.replace(' ', '')
+    n = len(encrypted_text)
+
+    middle = (n + 1) // 2
+
+    line1 = encrypted_text[:middle]
+    line2 = encrypted_text[middle:]
+
+    res = ''
+
+    i, j = 0, 0
+
+    for index in range(n):
+        if index % 2 == 0:
+            res += line1[i]
+            i += 1
+        else:
+            res += line2[j]
+            j += 1
+
+    arr = list(res)
+    for pos in spaces:
+        arr.insert(pos, ' ')
+
+    res = ''.join(arr)
+    return res

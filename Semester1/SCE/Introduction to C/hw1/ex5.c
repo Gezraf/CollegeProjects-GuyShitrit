a89a4092fdcc24ccab9bbd32ad4aea0ea9a7b9de
Gezraf <59087430+Gezraf@users.noreply.github.com>
2026-06-18 19:33:02 +0300

    Update ex5.c

--- Semester1/SCE/Introduction to C/hw1/ex5.c
+++ Semester1/SCE/Introduction to C/hw1/ex5.c
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 /* Assignment: 5
-Author: Guy Shitrit, ID: 330707761
+Author: Guy Shitrit
 */
 
 #include <stdio.h>
@@ -36,43 +35,4 @@ int ex5() {
     }
 
     return 0;
-=======
-/* Assignment: 5
-Author: Guy Shitrit, ID: 330707761
-*/
-
-#include <stdio.h>
-
-int ex5() {
-    int h1, d1;
-    int h2, d2;
-
-    printf("Enter h1 and d1: ");
-    scanf("%d%d", &h1, &d1);
-
-    printf("Enter h2 and d2: ");
-    scanf("%d%d", &h2, &d2);
-
-    if (h1 < h2) { // אם השעה הראשונה קטנה מהשעה השנייה
-        if (d1 <= d2) {
-            printf("difH=%d, difD=%d", (h2-h1), (d2-d1));
-        } else {
-            printf("difH=%d, difD=%d", (h2 - h1) - 1, 60 - d1 + d2);
-        }
-    } else if (h1 == h2) { // אם השעה הראשונה שווה לשעה השנייה
-        if (d1 <= d2) {
-            printf("difH=%d, difD=%d", 0, (d2-d1));
-        } else {
-            printf("difH=%d, difD=%d", 23, 60 - d1 + d2); // כאן זה 23 קבוע משום ששני השעות שוות אך המספר השני של הדקות קטן מהמספר הראשון
-        }
-    } else { // h1 > h2 אם השעה הראשונה גדולה מהשעה השנייה
-        if (d1 <= d2) {
-            printf("difH=%d, difD=%d", 24 - (h1 - h2), (d2-d1));
-        } else {
-            printf("difH=%d, difD=%d", 24 - (h1 - h2) - 1, 60 - d1 + d2);
-        }
-    }
-
-    return 0;
->>>>>>> 7f3c390aaa48ef5e58dfc4f69473c5416db68068
 }

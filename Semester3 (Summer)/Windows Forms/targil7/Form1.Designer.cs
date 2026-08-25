d2f1e5cc1d2abc35bda3d7b5e39d1bb93ef73796
Gezraf <59087430+Gezraf@users.noreply.github.com>
2026-08-25 15:09:24 +0300

    Add files via upload

--- Semester3 (Summer)/Windows Forms/targil7/Form1.Designer.cs
+++ Semester3 (Summer)/Windows Forms/targil7/Form1.Designer.cs
@@ -7,8 +7,16 @@ namespace targil7
     {
         private System.ComponentModel.IContainer components = null;
 
-        private Label lblTitle;
-        private FlowLayoutPanel pnlGallery;
+        private Label lblName;
+        private Label lblGrade;
+
+        private ListView lstStudents;
+        private ListView lstPictures;
+
+        private TextBox txtStudentName;
+
+        private Button btnFindStudent;
+        private Button btnShowAll;
 
         protected override void Dispose(bool disposing)
         {
@@ -20,46 +28,104 @@ namespace targil7
 
         private void InitializeComponent()
         {
-            this.lblTitle = new Label();
-            this.pnlGallery = new FlowLayoutPanel();
+            this.lblName = new Label();
+            this.lblGrade = new Label();
+
+            this.lstStudents = new ListView();
+            this.lstPictures = new ListView();
+
+            this.txtStudentName = new TextBox();
+
+            this.btnFindStudent = new Button();
+            this.btnShowAll = new Button();
 
             this.SuspendLayout();
 
             //
-            // lblTitle
+            // lblName
             //
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font =
-                new Font("Microsoft Sans Serif", 18F, FontStyle.Bold);
+            this.lblName.AutoSize = true;
+            this.lblName.Font =
+                new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            this.lblName.Location = new Point(65, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Text = "Name";
 
-            this.lblTitle.Location = new Point(30, 20);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Text = "Students Gallery";
+            //
+            // lblGrade
+            //
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Font =
+                new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            this.lblGrade.Location = new Point(245, 30);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Text = "Grade";
 
             //
-            // pnlGallery
+            // lstStudents
             //
-            this.pnlGallery.AutoScroll = true;
-            this.pnlGallery.BorderStyle = BorderStyle.FixedSingle;
-            this.pnlGallery.Location = new Point(30, 70);
-            this.pnlGallery.Name = "pnlGallery";
-            this.pnlGallery.Size = new Size(1000, 600);
-            this.pnlGallery.TabIndex = 0;
-            this.pnlGallery.WrapContents = true;
+            this.lstStudents.Location = new Point(30, 60);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new Size(330, 390);
+            this.lstStudents.View = View.List;
+
+            //
+            // txtStudentName
+            //
+            this.txtStudentName.Location = new Point(75, 480);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new Size(240, 22);
+
+            //
+            // btnFindStudent
+            //
+            this.btnFindStudent.Location = new Point(95, 525);
+            this.btnFindStudent.Name = "btnFindStudent";
+            this.btnFindStudent.Size = new Size(200, 45);
+            this.btnFindStudent.Text = "Find Student";
+            this.btnFindStudent.UseVisualStyleBackColor = true;
+            this.btnFindStudent.Click +=
+                new System.EventHandler(this.btnFindStudent_Click);
+
+            //
+            // lstPictures
+            //
+            this.lstPictures.Location = new Point(410, 30);
+            this.lstPictures.Name = "lstPictures";
+            this.lstPictures.Size = new Size(700, 490);
+            this.lstPictures.View = View.LargeIcon;
+
+            //
+            // btnShowAll
+            //
+            this.btnShowAll.Location = new Point(660, 545);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new Size(200, 50);
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click +=
+                new System.EventHandler(this.btnShowAll_Click);
 
             //
             // Form1
             //
             this.AutoScaleDimensions = new SizeF(8F, 16F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(1060, 700);
+            this.ClientSize = new Size(1140, 630);
 
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.pnlGallery);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblGrade);
+
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.btnFindStudent);
+
+            this.Controls.Add(this.lstPictures);
+            this.Controls.Add(this.btnShowAll);
 
             this.Name = "Form1";
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "Students Gallery";
+            this.Text = "Students";
 
             this.ResumeLayout(false);
             this.PerformLayout();
--- Semester3 (Summer)/Windows Forms/targil7/Form1.cs
+++ Semester3 (Summer)/Windows Forms/targil7/Form1.cs
@@ -11,27 +11,39 @@ namespace targil7
         string connectionString =
             @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolDB;Integrated Security=True";
 
+        ImageList studentImages = new ImageList();
+
         public Form1()
         {
             InitializeComponent();
-            ShowStudents();
+
+            studentImages.ImageSize = new Size(150, 150);
+            studentImages.ColorDepth = ColorDepth.Depth32Bit;
+
+            lstPictures.LargeImageList = studentImages;
+            lstPictures.View = View.LargeIcon;
         }
 
-        private void ShowStudents()
+        private void btnShowAll_Click(object sender, EventArgs e)
         {
+            lstPictures.Items.Clear();
+            studentImages.Images.Clear();
+
             SqlConnection con =
                 new SqlConnection(connectionString);
 
             con.Open();
 
             string sql =
-                "SELECT fname, grade, picture FROM studentsPictures";
+                "SELECT picture FROM studentsPictures";
 
             SqlCommand cmd =
                 new SqlCommand(sql, con);
 
             SqlDataReader reader =
                 cmd.ExecuteReader();
+
+            int imageIndex = 0;
 
             while (reader.Read())
             {
@@ -40,22 +52,62 @@ namespace targil7
 
                 if (File.Exists(picturePath))
                 {
-                    PictureBox pic = new PictureBox();
-
-                    pic.Width = 220;
-                    pic.Height = 220;
-
-                    pic.Image =
+                    Image image =
                         Image.FromFile(picturePath);
 
-                    pic.SizeMode =
-                        PictureBoxSizeMode.Zoom;
+                    studentImages.Images.Add(image);
 
-                    pic.Margin =
-                        new Padding(15);
+                    string student =
+                        reader["fname"].ToString() +
+                        "                         " +
+                        reader["grade"].ToString();
 
-                    pnlGallery.Controls.Add(pic);
+                    lstStudents.Items.Add(student);
+
+                    imageIndex++;
                 }
+            }
+
+            reader.Close();
+            con.Close();
+        }
+
+        private void btnFindStudent_Click(object sender, EventArgs e)
+        {
+            lstStudents.Items.Clear();
+
+            string name =
+                txtStudentName.Text;
+
+            SqlConnection con =
+                new SqlConnection(connectionString);
+
+            con.Open();
+
+            string sql =
+                "SELECT fname, grade FROM studentsPictures " +
+                "WHERE fname = @fname";
+
+            SqlCommand cmd =
+                new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue(
+                "@fname",
+                name);
+
+            SqlDataReader reader =
+                cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListViewItem item =
+                    new ListViewItem(
+                        reader["fname"].ToString());
+
+                item.SubItems.Add(
+                    reader["grade"].ToString());
+
+                lstStudents.Items.Add(item);
             }
 
             reader.Close();

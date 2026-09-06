900d25f65d16af91b17d1db85c8ce258b161db37
Gezraf <59087430+Gezraf@users.noreply.github.com>
2026-09-06 15:40:22 +0300

    Add files via upload

---
+++ Semester3 (Summer)/Windows Forms/targil8/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace targil8
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private Button btnFlip;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnFlip = new Button();
+
+            this.SuspendLayout();
+
+            //
+            // btnFlip
+            //
+            this.btnFlip.Location = new Point(325, 500);
+            this.btnFlip.Name = "btnFlip";
+            this.btnFlip.Size = new Size(150, 50);
+            this.btnFlip.Text = "Flip";
+            this.btnFlip.UseVisualStyleBackColor = true;
+
+            this.btnFlip.Click +=
+                new System.EventHandler(this.btnFlip_Click);
+
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new SizeF(8F, 16F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+
+            this.ClientSize = new Size(800, 600);
+
+            this.Controls.Add(this.btnFlip);
+
+            this.Name = "Form1";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Image Flip";
+
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Semester3 (Summer)/Windows Forms/targil8/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace targil8
+{
+    public partial class Form1 : Form
+    {
+        string imagePath = @"C:\Users\lolex\Downloads\logo.png";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackgroundImage = Image.FromFile(imagePath);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btnFlip_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage.RotateFlip(
+                RotateFlipType.RotateNoneFlipX
+            );
+
+            this.Invalidate();
+        }
+    }
+}
